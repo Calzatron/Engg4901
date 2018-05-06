cdb5b6b7859c1d2d5c31382a6c9e989ff620718a
calzatron <callumrohweder@gmail.com>
2018-05-06 17:03:33 +1000

    Thesis'ing

--- RoboticsToolboxInverseKinematics.m
+++ RoboticsToolboxInverseKinematics.m
@@ -9,3 +9,15 @@ T = jaco.fkine(q)
 qi = jaco.ikine(T, 'pinv')
 
 jaco.fkine(qi)
+
+
+%% Move the arm to a specific location and angle
+T = transl(0.3, 0.1, 0) * rpy2tr(0, 0, 0, 'deg')
+% plot the new coordinate
+
+%calculate the angles to move
+q = jaco.ikine(T, qz, [1 1 1 0 0 0]);   % initial angles, and xyz coordinate match
+% show angles
+jaco.plot(q);%*pi/180)
+hold on;
+trplot(T)
